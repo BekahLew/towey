42842629223c547e10155f810b0d32456e8ce958
BekahLew <rebekah@efellemedia.com>
2017-06-05 08:44:33 -0700

    ^ Updating about page structure and masthead image. Scroll opacity only on homepage masthead

--- js/agency.js
+++ js/agency.js
@@ -33,7 +33,7 @@
     });
 
 $(window).scroll(function(){
-    $(".top").css("opacity", 1 - $(window).scrollTop() / 800);
+    $(".frontpage .top").css("opacity", 1 - $(window).scrollTop() / 800);
   });
 
 /*win.scroll(function(){
